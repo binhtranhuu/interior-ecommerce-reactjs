bc7aa2c4f23ad614d6ac07c64665bbf88f52ab1b
unknown <tranbinh241999@gmail.com>
2022-02-27 12:38:12 +0700

    Implement Signin and Signup Backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,80 @@
+import bcrypt from 'bcryptjs';
+import isEmail from 'validator/lib/isEmail.js';
 import User from '../models/userModel.js';
 import data from '../data.js';
+import { generateToken } from '../utils/authMiddleware.js';
 
 export const createSampleUsers = async (req, res) => {
   try {
     await User.remove({});
     const createdUsers = await User.insertMany(data.users);
-    res.send({ createdUsers });
+    return res.send({ createdUsers });
   } catch (error) {
-    res.status(500).send({ message: 'Import Users Failed' });
+    return res.status(500).send({ message: 'Import Users Failed' });
+  }
+};
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password, isAdmin = false } = req.body;
+
+    if (!isEmail(email)) return res.status(401).send({ message: 'Invalid Email' });
+
+    if (password.length < 6) {
+      return res.status(401).send({ message: 'Password must be atleast 6 characters' });
+    }
+
+    const isUser = await User.findOne({ email: email.toLowerCase() });
+
+    if (isUser) {
+      return res.status(401).send({ message: 'User already registered' });
+    }
+
+    const user = new User({
+      name: name,
+      email: email.toLowerCase(),
+      isAdmin: isAdmin,
+      password: bcrypt.hashSync(password, 10),
+    });
+    const createdUser = await user.save();
+
+    return res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    });
+  } catch (error) {
+    return res.status(500).send({ message: 'An error occurred. Please try again later' });
+  }
+};
+
+export const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!isEmail(email)) return res.status(401).send({ message: 'Invalid Email' });
+
+    if (password.length < 6) {
+      return res.status(401).send({ message: 'Password must be atleast 6 characters' });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      if (bcrypt.compareSync(password, user.password)) {
+        return res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+      }
+    }
+    return res.status(401).send({ message: 'Invalid email or password' });
+  } catch (error) {
+    return res.status(500).send({ message: 'An error occurred. Please try again later' });
   }
 };
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -6,4 +6,10 @@ const userRouter = express.Router();
 // Create sample users
 userRouter.get('/seed', createSampleUsers);
 
+// Signup user
+userRouter.post('/signup', signup);
+
+// Signin user
+userRouter.post('/signin', signin);
+
 export default userRouter;
